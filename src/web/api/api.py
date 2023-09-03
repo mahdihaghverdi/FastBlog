1e0d0dc813ec9c61cb7f9ee25237c1d26c28b681
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-03 14:44:12 +0330

    Write the skeleton of GET '/posts'

--- src/web/api/api.py
+++ src/web/api/api.py
@@ -9,14 +9,25 @@ from src.web.app import app
 @app.get("/posts")
 async def get_posts(
     page: Annotated[
-        int | None,
-        Query(description="page number of the pagination", ge=1),
+        int,
+        Query(
+            description="page number of the pagination",
+            ge=1,
+        ),
     ] = 1,
     per_page: Annotated[
-        int | None,
-        Query(alias="per-page", description="number of posts per page", ge=1, le=30),
+        int,
+        Query(
+            alias="per-page",
+            description="number of posts per page",
+            ge=1,
+            le=30,
+        ),
     ] = 5,
-    sort: Annotated[Sort, Query(description="sorts the returned posts")] = Sort.DATE,
+    sort: Annotated[
+        Sort,
+        Query(description="sorts the returned posts"),
+    ] = Sort.DATE,
     order: Annotated[
         Order,
         Query(description="order of the sorted posts"),
