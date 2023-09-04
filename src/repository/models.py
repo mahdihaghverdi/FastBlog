cd539c52fc7fa0ae1f555d49b3695e7f5ad2e246
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-04 16:13:29 +0330

    implement the PostModel model

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,5 +1,27 @@
-from sqlalchemy.orm import DeclarativeBase
+import uuid
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from src.web.api.schemas import PostSchema
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class PostModel(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    created: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    title: Mapped[str]
+    body: Mapped[str]
+
+    def dict(self):
+        return PostSchema(
+            id=self.id,
+            created=self.created,
+            title=self.title,
+            body=self.body,
+        ).model_dump(mode="json")
