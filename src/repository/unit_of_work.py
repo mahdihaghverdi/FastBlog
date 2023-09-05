e36b7155a4041ee2e5b9e8baaca9e08d6fac4500
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-05 11:05:26 +0330

    Implement the Unit of Work

---
+++ src/repository/unit_of_work.py
@@ -0,0 +1,15 @@
+class UnitOfWork:
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = await self.session_factory()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
