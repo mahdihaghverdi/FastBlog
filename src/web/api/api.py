fa837dedb359218591da51c5172cd01a5dc2e9e7
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-05 12:55:04 +0330

    complete the implementation of the routes

--- src/web/api/api.py
+++ src/web/api/api.py
@@ -1,10 +1,15 @@
 from typing import Annotated
 
-from fastapi import Query
+from fastapi import Query, Depends
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from starlette import status
 
+from src.repository.posts_repository import PostsRepository
+from src.repository.unit_of_work import UnitOfWork
+from src.service.posts_service import PostsService
 from src.web.api.schemas import Sort, PostSchema, CreatePostSchema
 from src.web.app import app
+from src.web.dependencies import get_async_sessionmaker
 
 
 @app.get(
@@ -13,12 +18,10 @@ from src.web.app import app
     status_code=status.HTTP_200_OK,
 )
 async def get_posts(
+    asessionmaker: Annotated[async_sessionmaker, Depends(get_async_sessionmaker)],
     page: Annotated[
         int,
-        Query(
-            description="page number of the pagination",
-            ge=1,
-        ),
+        Query(description="page number of the pagination", ge=1),
     ] = 1,
     per_page: Annotated[
         int,
@@ -39,6 +42,16 @@ async def get_posts(
     ] = True,
 ):
     """Retrieve all the posts"""
+    async with UnitOfWork(asessionmaker) as uow:
+        repo = PostsRepository(uow.session)
+        service = PostsService(repo)
+        posts = await service.list_posts(
+            page=page,
+            per_page=per_page,
+            sort=sort,
+            desc_=desc,
+        )
+        return posts
 
 
 @app.post(
@@ -46,5 +59,16 @@ async def get_posts(
     response_model=PostSchema,
     status_code=status.HTTP_201_CREATED,
 )
-async def create_post(post: CreatePostSchema):
+async def create_post(
+    post: CreatePostSchema,
+    asessionmaker: Annotated[async_sessionmaker, Depends(get_async_sessionmaker)],
+):
     """Create a post"""
+    async with UnitOfWork(asessionmaker) as uow:
+        repo = PostsRepository(uow.session)
+        service = PostsService(repo)
+        post = await service.create_post(post.model_dump())
+        await uow.commit()
+        payload = await post.dict()
+
+    return payload
