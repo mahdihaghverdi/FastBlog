ffe204ad384dd6ac4d5715c709ee554d14bbcdaa
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-05 12:51:50 +0330

    remove the protected attributes and properties. resolve the `id` and `created` using model.awaitable_attrs

--- src/service/posts.py
+++ src/service/posts.py
@@ -1,15 +1,15 @@
 class Post:
-    def __init__(self, *, id, created, title, body, post_model):
-        self._id = id
-        self._created = created
+    def __init__(self, *, id, created, title, body, post_model=None):
+        self.id = id
+        self.created = created
         self.title = title
         self.body = body
         self.post_model = post_model
 
-    @property
-    def id(self):
-        return self._id or self.post_model.id
-
-    @property
-    def created(self):
-        return self._created or self.post_model.created
+    async def dict(self):
+        return {
+            "id": self.id or (await self.post_model.awaitable_attrs.id),
+            "created": self.created or (await self.post_model.awaitable_attrs.created),
+            "title": self.title,
+            "body": self.body,
+        }
