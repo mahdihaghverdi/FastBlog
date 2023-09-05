6f9941128a9ed4215c1285dbfedf4b4868b38a87
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-05 12:50:36 +0330

    define `get_database_url`, `get_db_engine` and `get_async_sessionmaker` dependency

--- src/web/dependencies.py
+++ src/web/dependencies.py
@@ -1,4 +1,8 @@
 from functools import lru_cache
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
 
 from src.web.config import Settings
 
@@ -6,3 +10,17 @@ from src.web.config import Settings
 @lru_cache
 def get_settings() -> Settings:
     return Settings()
+
+
+def get_database_url(settings: Annotated[Settings, Depends(get_settings)]) -> str:
+    return str(settings.database_url)
+
+
+def get_db_engine(url: Annotated[str, Depends(get_database_url)]) -> AsyncEngine:
+    return create_async_engine(url)
+
+
+def get_async_sessionmaker(
+    db: Annotated[AsyncEngine, Depends(get_db_engine)],
+) -> async_sessionmaker:
+    return async_sessionmaker(db, expire_on_commit=False)
