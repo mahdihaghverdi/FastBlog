c09f0eb5f389a2a66467625336d3cad5a6968064
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-05 22:05:23 +0330

    write the test for GET '/posts'

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 def test_create_a_post(client):
     title, body = "Python 3.11", "Wow, such a release!"
     response = client.post(
@@ -21,3 +25,58 @@ def test_create_a_post(client):
     assert post["created"] == post_data["created"]
     assert post["title"] == post_data["title"]
     assert post["body"] == post_data["body"]
+
+
+def random_string():
+    return "".join(random.choices(string.ascii_lowercase, k=1))
+
+
+def test_list_posts(client):
+    posts = [
+        [{"title": random_string(), "body": random_string()} for _ in range(5)]
+        for _ in range(5)
+    ]
+    posts = [
+        client.post("/posts", json=post).json()
+        for post_list in posts
+        for post in post_list
+    ]
+
+    # default query params: page = 1, per-page = 5, sort = date, desc = true
+    # pages
+    response = client.get("/posts")
+    assert response.status_code == 200, response.text
+    assert len(response.json()) == 5
+
+    response = client.get("/posts", params={"page": 2})
+    assert len(response.json()) == 5
+
+    response = client.get("/posts", params={"page": 3})
+    assert len(response.json()) == 5
+
+    response = client.get("/posts", params={"page": 4})
+    assert len(response.json()) == 5
+
+    response = client.get("/posts", params={"page": 5})
+    assert len(response.json()) == 5
+
+    # per-page
+    response = client.get("/posts", params={"per-page": 10})
+    assert len(response.json()) == 10
+
+    response = client.get("/posts", params={"per-page": 20})
+    assert len(response.json()) == 20
+
+    response = client.get("/posts", params={"per-page": len(posts)})
+    assert len(response.json()) == len(posts)
+
+    # sort
+    response = client.get("/posts", params={"per-page": len(posts)})
+    assert response.json() == posts[::-1]
+
+    response = client.get("/posts", params={"per-page": len(posts), "sort": "title"})
+    assert response.json() == sorted(posts, key=lambda x: x["title"], reverse=True)
+
+    # desc
+    response = client.get("/posts", params={"per-page": len(posts), "desc": "false"})
+    assert response.json() == posts
