bbfd21caaa9f97d8522af916c847392e8a9c08b6
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-07 15:25:49 +0330

    update `test_create_post` function

--- tests/test_success_web.py
+++ tests/test_success_web.py
@@ -3,28 +3,17 @@ import string
 
 
 def test_create_post(client):
-    title, body = "Python 3.11", "Wow, such a release!"
-    response = client.post(
-        "/posts",
-        json={
-            "title": title,
-            "body": body,
-        },
-    )
+    payload = {
+        "title": "Python 3.11",
+        "body": "Wow, such a release!",
+    }
+    response = client.post("/posts", json=payload)
     assert response.status_code == 201, response.text
 
     post_data = response.json()
-    assert post_data["title"] == title
-    assert post_data["body"] == body
-
     response = client.get("/posts")
-    assert response.status_code == 200, response.text
-
     post = response.json()[0]
-    assert post["id"] == post_data["id"]
-    assert post["created"] == post_data["created"]
-    assert post["title"] == post_data["title"]
-    assert post["body"] == post_data["body"]
+    assert post == post_data
 
 
 def random_string():
