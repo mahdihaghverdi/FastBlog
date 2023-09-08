e771a7b825346dd206f4b5cd150e763a5669d054
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-08 17:17:58 +0330

    add the new needed env vars to Settings model

--- src/web/config.py
+++ src/web/config.py
@@ -1,9 +1,12 @@
-from pydantic import PostgresDsn
+from pydantic import PostgresDsn, SecretStr
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     database_url: PostgresDsn | str
+    secret_key: SecretStr
+    algorithm: str
+    access_token_expire_minutes: int = 1 * 24 * 60  # one day
 
 
 settings = Settings()
