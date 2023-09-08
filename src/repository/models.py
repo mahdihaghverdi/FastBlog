3f9539d13414355959f4201529e6e1cfe30a4c7b
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-08 18:04:13 +0330

    define the UserModel and create the one-to-many rel with PostModel

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,21 +1,38 @@
-import uuid
 from datetime import datetime
+from uuid import uuid4, UUID
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    username: Mapped[str]
+    password: Mapped[str]
+
+    posts: Mapped[list["PostModel"]] = relationship(
+        back_populates="user",
+        cascade="delete, delete-orphan",
+    )
+
+
 class PostModel(Base):
     __tablename__ = "posts"
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     created: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     title: Mapped[str]
     body: Mapped[str]
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["UserModel"] = relationship(back_populates="posts")
 
     async def dict(self):
         return {
