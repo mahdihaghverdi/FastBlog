6f44b6885a7b6ff0371103db4bd540c96d46364a
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-08 23:36:51 +0330

    implement UserRepository with its `get` method

---
+++ src/repository/users_repository.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+
+from src.repository import BaseRepository
+from src.repository.models import UserModel
+from src.service.posts import BaseBusinessObject
+from src.service.users import User
+
+
+class UserRepository(BaseRepository):
+    async def get(self, id_: UUID) -> BaseBusinessObject | None:
+        user = await self._get(UserModel, id_)
+        if user is not None:
+            return User(**(await user.dict()))
+
+    async def add(self, data: dict) -> BaseBusinessObject:
+        pass
+
+    async def update(self, id_: UUID, data: dict) -> BaseBusinessObject | None:
+        pass
+
+    async def delete(self, id_: UUID) -> BaseBusinessObject | None:
+        pass
+
+    async def list(self, **kwargs) -> list[BaseBusinessObject]:
+        pass
