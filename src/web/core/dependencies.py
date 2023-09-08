c6a443ad1006d472cacfc3fa936e1c98b5a52a18
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-08 23:51:21 +0330

    add oauth2_scheme to dependencies

--- src/web/core/dependencies.py
+++ src/web/core/dependencies.py
@@ -1,7 +1,12 @@
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError  # noqa
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from src.web.core.database import sqlalchemy_engine
 
 
-def get_async_sessionmaker() -> async_sessionmaker:
+async def get_async_sessionmaker() -> async_sessionmaker:
     return async_sessionmaker(sqlalchemy_engine, expire_on_commit=False)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="access-token")
