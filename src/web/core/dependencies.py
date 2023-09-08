948521486707fece3a771223341bf113e23142f2
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-08 23:53:23 +0330

    implement `get_current_user` dependency

--- src/web/core/dependencies.py
+++ src/web/core/dependencies.py
@@ -1,14 +1,16 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError  # noqa
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from starlette import status
 
 from src.repository.unit_of_work import UnitOfWork
 from src.repository.user_repository import UserRepository
 from src.service.user_service import UserService
 from src.service.users import User
+from src.web.core.config import settings
 from src.web.core.database import sqlalchemy_engine
 
 
@@ -19,7 +21,7 @@ async def get_async_sessionmaker() -> async_sessionmaker:
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="access-token")
 
 
-def get_user(
+async def get_user(
     asession: Annotated[async_sessionmaker, Depends(get_async_sessionmaker)],
     user_id,
 ) -> User:
@@ -27,3 +29,27 @@ def get_user(
         repo = UserRepository(uow.session)
         service = UserService(repo)
         return await service.get_user(user_id)
+
+
+async def get_current_user(
+    asession: Annotated[async_sessionmaker, Depends(get_async_sessionmaker)],
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token=token,
+            key=settings.secret_key,
+            algorithms=[settings.algorithm],
+        )
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(asession, user_id)
+    return user
