ff12246e49a7d9ea2dc540ca88cd85266521494f
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-09 16:48:38 +0330

    most posts tests to post dir. remove unnecessary mock function

--- tests/posts/conftest.py
+++ tests/posts/conftest.py
@@ -3,14 +3,12 @@ import pathlib
 import sys
 
 import pytest
-from sqlalchemy.ext.asyncio import async_sessionmaker
 from starlette.testclient import TestClient
 
-sys.path.append(str(pathlib.Path(__file__).parent.parent))
+sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
 from src.repository.models import Base  # noqa: E402
 from src.web.app import app  # noqa: E402
 from src.web.core.database import sqlalchemy_engine as engine  # noqa: E402
-from src.web.core.dependencies import get_async_sessionmaker  # noqa: E402
 
 
 async def create_all():
@@ -23,20 +21,9 @@ async def drop_all():
         await conn.run_sync(Base.metadata.drop_all)
 
 
-def get_async_sessionmaker_mock():
-    testing_session = async_sessionmaker(
-        bind=engine,
-        autoflush=False,
-        autocommit=False,
-        expire_on_commit=False,
-    )
-    return testing_session
-
-
 @pytest.fixture(scope="function")
 def client() -> TestClient:
     asyncio.run(create_all())
-    app.dependency_overrides[get_async_sessionmaker] = get_async_sessionmaker_mock
     try:
         yield TestClient(app=app)
     finally:
