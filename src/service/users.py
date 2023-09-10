4cc673d5a528c744ae49cb75afb631819ea299cb
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-10 21:01:33 +0330

    add `draft_posts` to User dto

--- src/service/users.py
+++ src/service/users.py
@@ -2,11 +2,12 @@ from src.service import BaseBusinessObject
 
 
 class User(BaseBusinessObject):
-    def __init__(self, *, id, username, password, posts, user_model=None):
+    def __init__(self, *, id, username, password, posts, draft_posts, user_model=None):
         self.id = id
         self.username = username
         self.password = password
         self.posts = posts
+        self.draft_posts = draft_posts
         self.user_model = user_model
 
     async def dict(self):
@@ -15,4 +16,5 @@ class User(BaseBusinessObject):
             "username": self.username,
             "password": self.password,
             "posts": self.posts or (await self.user_model.dict())["posts"],
+            "draft_posts": self.posts or (await self.user_model.dict())["draft_posts"],
         }
