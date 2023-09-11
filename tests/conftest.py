cfec327cbdea20e38bf48cedc113a6b216907dd8
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-11 17:14:02 +0330

    signup_and_auth moved to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ import sys
 import pytest
 from starlette.testclient import TestClient
 
+
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from src.web.core.config import settings  # noqa: E402
 from src.repository.models import Base  # noqa: E402
 from src.web.app import app  # noqa: E402
 from src.web.core.database import sqlalchemy_engine as engine  # noqa: E402
@@ -19,6 +21,16 @@ async def create_all():
 async def drop_all():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+def signup_and_auth(client, username="string", password="password"):
+    client.post("/users/signup", data={"username": username, "password": password})
+    access_token = client.post(
+        f"/auth/{settings.access_token_url}",
+        data={"grant_type": "password", "username": username, "password": password},
+    ).json()["access_token"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+    return headers
 
 
 @pytest.fixture(scope="function")
--- tests/posts/test_fail.py
+++ tests/posts/test_fail.py
@@ -1,22 +1,31 @@
 import uuid
 
+from tests.conftest import signup_and_auth
+
 
 def test_get_post(client):
     post_id = uuid.uuid4()
-    response = client.get(f"/posts/{post_id}")
+    headers = signup_and_auth(client)
+    response = client.get(f"/posts/{post_id}", headers=headers)
     assert response.status_code == 404, response.text
     assert response.json()["detail"] == f"post with id: '{post_id}' is not found"
 
 
 def test_update_post(client):
     post_id = uuid.uuid4()
-    response = client.put(f"/posts/{post_id}", json={"title": "a", "body": "b"})
+    headers = signup_and_auth(client)
+    response = client.put(
+        f"/posts/{post_id}",
+        json={"title": "a", "body": "b"},
+        headers=headers,
+    )
     assert response.status_code == 404, response.text
     assert response.json()["detail"] == f"post with id: '{post_id}' is not found"
 
 
 def test_delete_post(client):
     post_id = uuid.uuid4()
-    response = client.delete(f"/posts/{post_id}")
+    headers = signup_and_auth(client)
+    response = client.delete(f"/posts/{post_id}", headers=headers)
     assert response.status_code == 404, response.text
     assert response.json()["detail"] == f"post with id: '{post_id}' is not found"
