989b2e02a5c810bf93fa1a86d798dc3d0df272f2
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-12 23:47:46 +0330

    test global posts

---
+++ tests/test_globals.py
@@ -0,0 +1,8 @@
+def test_get_global_post(client, payload, headers):
+    post = client.post("/posts", json=payload, headers=headers).json()
+
+    response = client.get(post["url"])
+    assert response.status_code == 200, response.text
+
+    assert post["title"] == payload["title"]
+    assert post["body"] == payload["body"]
