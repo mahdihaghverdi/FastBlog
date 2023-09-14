4d6b14f5a762b755f8b8c64ea4c9f7446a8ffa6b
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-14 21:34:33 +0330

    add tag to post schemas

--- src/web/core/schemas/post.py
+++ src/web/core/schemas/post.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
-from pydantic import BaseModel, constr, AnyHttpUrl
+from pydantic import BaseModel, constr, AnyHttpUrl, conlist
+
+from src.web.core.schemas.tag import TagSchema
 
 
 class CreatePostSchema(BaseModel):
     title: constr(strip_whitespace=True, min_length=1)
     body: constr(strip_whitespace=True, min_length=1)
+    tags: conlist(TagSchema, min_length=1, max_length=5, unique_items=True)
     title_in_url: constr(strip_whitespace=True, min_length=1) | None = None
 
 
@@ -17,3 +20,4 @@ class PostSchema(BaseModel):
     url: AnyHttpUrl
     # the pattern for posts' url is like this: https://fastblog.io/@username/slugged-title-somehash
     # this is generated automatically for posts that'll be published
+    tags: list[TagSchema]
