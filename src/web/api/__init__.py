03c99bc8e0a7da3e5f49fc5a080846b388a8572f
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-09-29 17:33:10 +0330

    add support for list if dicts for giving domains

--- src/web/api/__init__.py
+++ src/web/api/__init__.py
@@ -5,7 +5,10 @@ def give_domain(domain, /, post_or_posts):
     if isinstance(post_or_posts, list):
         posts = post_or_posts
         for post in posts:
-            post.url = str(urlpath.URL(domain) / post.url)
+            if isinstance(post, dict):
+                post["url"] = str(urlpath.URL(domain) / post["url"])
+            else:
+                post.url = str(urlpath.URL(domain) / post.url)
         return posts
     post = post_or_posts
     if isinstance(post, dict):
